b4b6ae22e05546ad7267d3f43959dc4676126849
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-10-23 23:21:09 +0300

    [boinc_stats] Get total BOINC credit

--- BoincStats.py
+++ BoincStats.py
@@ -1,3 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+
 class BoincStats:
     """A class to get Boinc statistics for a CPID from BAM!"""
     cpid = None
@@ -10,5 +14,22 @@ class BoincStats:
                          cpid + "/"
 
     def get_stats(self):
-        # todo: missing implementation
-        return [self.cpid]
+        # Load main stats page
+        r = requests.get(self.stats_url)
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        stats_table = soup.find("table", id="tblStats")
+        total_credit_td = stats_table \
+            .find("td", string="Current Credit based on incremental update") \
+            .parent \
+            .find_all("td")[1]
+        # Get number (first line, before <br/>) and remove commas
+        total_credit_str = total_credit_td.contents[0].replace(",", "")
+
+        # Parse the number as a float
+        total_credit = float(total_credit_str)
+        print total_credit
+
+        return {
+            total_credit: total_credit
+        }
